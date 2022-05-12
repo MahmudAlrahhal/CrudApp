6fe8ed1fd56767d8da6af227ee36ef5896ca6979
Mahmud <Mahmud@DESKTOP-U25FK8I>
2022-05-12 17:00:40 +0300

    Add project files.

---
+++ DB.sql
@@ -0,0 +1,46 @@
+Create database MVCTutorial
+USE [MVCTutorial]
+GO
+/****** Object:  Table [dbo].[tblDepartment]    Script Date: 31-Jul-17 11:19:58 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tblDepartment](
+	[DepartmentId] [int] IDENTITY(1,1) NOT NULL,
+	[DepartmentName] [nvarchar](50) NULL,
+ CONSTRAINT [PK_tblDepartment] PRIMARY KEY CLUSTERED 
+(
+	[DepartmentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[tblStudent]    Script Date: 31-Jul-17 11:19:58 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[tblStudent](
+	[StudentId] [int] IDENTITY(101,1) NOT NULL,
+	[StudentName] [nvarchar](50) NULL,
+	[Surname] [nvarchar](50) NULL,
+	[Email] [nvarchar](50) NULL,
+	[BirthDate] [date] NULL,
+	[Phone] [nvarchar](50) NULL,
+	[Location] [nvarchar](50) NULL,
+	[Photo] [nvarchar](50) NULL,
+	[IsDeleted] [bit] NULL,
+	[DepartmentId] [int] NULL,
+ CONSTRAINT [PK_tblStudent] PRIMARY KEY CLUSTERED 
+(
+	[StudentId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[tblStudent]  WITH CHECK ADD  CONSTRAINT [FK_tblStudent_tblDepartment] FOREIGN KEY([DepartmentId])
+REFERENCES [dbo].[tblDepartment] ([DepartmentId])
+GO
+ALTER TABLE [dbo].[tblStudent] CHECK CONSTRAINT [FK_tblStudent_tblDepartment]
+GO
---
+++ JuniorCandidateCrudApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JuniorCandidateCrudApplication.MvcApplication" Language="C#" %>
